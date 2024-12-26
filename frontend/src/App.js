3b8a9f9562a466872f0a66138fe03fcc265b9163
goblin5000 <hammad.qsr2964@gmail.com>
2024-12-26 22:01:49 +0500

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Dev React App</h1>
+          <h1 className="App-title">dev1 React App</h1>
         </header>
           <Routes>
                 <Route exact path="/" element={<Navigate to="/customerlist" />} />
